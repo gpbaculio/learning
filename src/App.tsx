409525483e980d20824ca94ee30fa95a97e71f59
gpbaculio <gpbaculio@gmail.com>
2021-08-17 22:43:11 +0800

    complete card flip tutorial, flip on one direction
    https://www.youtube.com/watch?v=QGVXmoZWZuw

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import { Home, CardFlip } from "./screens";
+
+import "./screens/CardFlip/CardFlip.css";
+import "./App.css";
+import { ThemeProvider } from "styled-components";
+const theme = {
+  primary: "#FFCE00",
+  secondary: "#FE4880",
+  dark: "#212121",
+  light: "#F3F3F3",
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/card-flip' component={CardFlip} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
